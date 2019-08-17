8f1c2739acb6da9f28f399521e8a279fb889ca62
Simon Thelin <simon.thelin@drivec.se>
2019-08-17 12:00:31 +0200

    change-gzip-to-lzo-d

--- src/helpers/subprocesses/decompress.py
+++ src/helpers/subprocesses/decompress.py
@@ -3,7 +3,7 @@ from io import StringIO
 
 
 def decompress(file):
-    pipe = subprocess.Popen([f'{file} | gzip --stdout'], stdout=subprocess.PIPE, shell=True)
+    pipe = subprocess.Popen([f'{file} | lzo -d --stdout'], stdout=subprocess.PIPE, shell=True)
     helper = StringIO()
     helper.write(pipe.stdout.read())
     helper.seek(0)
