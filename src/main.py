3b4ad01f09e2bccfb9e057bd99175ef1d45b8df1
Simon Thelin <simon.thelin@yoyowallet.com>
2019-08-31 13:05:53 +0100

    DT-006-enable-docker-compose-default-bucket-with-data

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
+from environs import Env
 from src.spark_session import InitSpark
 from src.helpers.aws.distributed_read_s3 import DistributedS3Reader
 from src.modules.schemas import OnlineRetailSchema
 
+env = Env()
+env.read_env()
+
+
 def main():
+
     # Initialise SparkSession
     init_spark_session = InitSpark("berserkr", "spark-warehouse")
     default_spark_session = init_spark_session.spark_init()
@@ -16,11 +22,11 @@ def main():
 
     # TODO: read from env file another PR
     raw_rdd = dist_s3_reader.distributed_read_from_s3(
-        s3_bucket='rawdata',
-        endpoint_url='http://127.0.0.1:9000',
-        aws_access_key_id='AKIAIOSFODNN7EXAMPLE',
-        aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-        signature_version='s3v4',
+        s3_bucket=env('S3_BUCKET'),
+        endpoint_url=env('ENDPOINT_URL'),
+        aws_access_key_id=env('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'),
+        signature_version=env('SIGNATURE_VERSION'),
     )
 
     # TODO: Move to application that will be called raw_to_parquet
